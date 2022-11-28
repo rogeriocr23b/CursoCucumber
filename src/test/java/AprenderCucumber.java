e478cf229aac6d03bd5115d98d596455a01065bc
Rogerio Ramos <rogeriocr23@gmail.com>
2022-11-28 15:04:38 -0300

    commit inicial

---
+++ src/test/java/AprenderCucumber.java
@@ -0,0 +1,22 @@
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class AprenderCucumber {
+	
+	@Dado("que criei o arquivo corretamente")
+	public void que_criei_o_arquivo_corretamente() {
+	 
+	}
+
+	@Quando("executa-lo")
+	public void executa_lo() {
+	
+	}
+
+	@Entao("a especificacao deve finalizar com sucesso")
+	public void a_especificacao_deve_finalizar_com_sucesso() {
+	
+	}
+
+}
---
+++ target/test-classes/aprender_cucumber.feature
@@ -0,0 +1,13 @@
+# language: pt
+Funcionalidade: Aprender Cucumber
+
+
+Como um aluno
+quero aprender a utilizar Cucumber
+Para que eu possa automatizar criterios de aceitacao
+
+Cenario: Deve executar especificacao
+
+Dado que criei o arquivo corretamente
+Quando executa-lo
+Entao a especificacao deve finalizar com sucesso
